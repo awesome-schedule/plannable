f4366925875638ef1ba74a86aba7fbf7eca79498
hanzhi713 <hanzhi713@163.com>
2019-03-02 12:37:42 -0500

    update structure

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import csv
 import logging
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from new_sis.classAlgo import readData, DICT
+from collections import OrderedDict
 
 app = Flask(__name__)
+
+RECORDS_SHORT = OrderedDict()
 
 
 @app.route('/')
@@ -21,9 +24,18 @@ def get_semesters():
     return jsonify(semesters)
 
 
-@app.route('/api/classes')
+@app.route('/api/classes', methods=['GET', 'POST'])
 def get_classes():
-    pass
+    if request.method == "GET":
+        return jsonify(
+            {
+                'meta': {
+                    'attr_map': {0: 'department', 1: 'number', 2: 'section', 3: 'type', 4: 'units', 5: 'instructor', 6: 'days', 7: 'room', 8: 'title', 9: 'topic', 10: 'status', 11: 'enrollment', 12: 'enrollment_limit', 13: 'wait_list', 14: 'description'}
+                },
+                'data': RECORDS_SHORT
+            }
+        )
+    return "haha"
 
 
 @app.route('/<any_text>')
@@ -31,8 +43,15 @@ def default_handler(any_text):
     return render_template('errors/404.html')
 
 
+def to_short():
+    for k, v in DICT.items():
+        RECORDS_SHORT[k] = [course[:10] for course in v]
+
+
 if __name__ == "__main__":
     logging.info('Loading data...')
     readData()
+    to_short()
     logging.info('Running...')
+
     app.run(host='0.0.0.0', port=8000, debug=True)

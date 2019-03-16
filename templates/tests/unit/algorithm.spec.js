24c70915929ffa22b2539517284d59f2446f8787
Zichao <zichao2018@gmail.com>
2019-03-15 23:53:55 -0400

    update algorithm & packages

--- templates/tests/unit/algorithm.spec.js
+++ templates/tests/unit/algorithm.spec.js
@@ -1,20 +1,25 @@
 import AllRecords from '../../src/models/AllRecords';
 import data from './data.js';
-import { Algorithm } from '../../src/algorithm/Algorithm';
-import {} from 'jasmine';
+import { ScheduleGenerator } from '../../src/algorithm/ScheduleGenerator';
+import Schedule from '../../src/models/Schedule';
 
 const allRecords = new AllRecords(data);
 
-describe('Algorithm Test', () => {
+describe('ScheduleGenerator Test', () => {
     it('Data Validation', () => {
         expect(typeof data).toBe('object');
         const course = allRecords.getRecord('aas10201');
         expect(course.id[0]).toBe(10309);
     });
 
-    it('Algorithm', () => {
-        const algorithm = new Algorithm(allRecords);
-        expect(typeof algorithm.createSchedule).toBe('function');
+    it('ScheduleGenerator', () => {
+        const generator = new ScheduleGenerator(allRecords);
+        expect(typeof generator.createSchedule).toBe('function');
         expect('output').toBe('output');
+        const schedule = new Schedule();
+        schedule.All = {
+            cs21105: -1
+        };
+        generator.getSchedules(schedule);
     });
 });

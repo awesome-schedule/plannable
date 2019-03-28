ef7c0f982747f722a27d8bc1801accfa43a424b4
hanzhi713 <hanzhi713@163.com>
2019-03-28 14:55:27 -0400

    add travis config

--- templates/.eslintrc.js
+++ templates/.eslintrc.js
@@ -7,8 +7,9 @@ module.exports = {
     },
     extends: ['plugin:vue/recommended', '@vue/prettier'],
     rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-console': 'off',
         'prefer-const': 'error',
         'vue/require-default-prop': 'off',
         'no-var': 'error',

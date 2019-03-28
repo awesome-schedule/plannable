46778b279ce3778d6497f50fdea11d35cb50beca
hanzhi713 <hanzhi713@163.com>
2019-03-28 02:53:56 -0400

    remove mathjs dependency

--- templates/src/algorithm/ScheduleEvaluator.js
+++ templates/src/algorithm/ScheduleEvaluator.js
@@ -1,9 +1,21 @@
 // @ts-check
-import * as math from 'mathjs';
 /**
  * @typedef {{schedule: import("./ScheduleGenerator").RawSchedule, coeff: number}} ComparableSchedule
  */
 class ScheduleEvaluator {
+    /**
+     * calculate the population variance
+     * @param {number[]} args
+     */
+    static variance(args) {
+        let sum = 0;
+        let sumSq = 0;
+        for (let i = 0; i < args.length; i++) {
+            sum += args[i];
+            sumSq += args[i] ** 2;
+        }
+        return sumSq ** 2 / (args.length - 1) - (sum / args.length) ** 2;
+    }
     constructor() {
         /**
          * @type {ComparableSchedule[]}
@@ -26,28 +38,26 @@ class ScheduleEvaluator {
         let fr = 0;
         for (const course of timeTable) {
             const minutes = course[2][1] - course[2][0];
-            if (course[1].indexOf('Mo') != -1) {
+            if (course[1].indexOf('Mo') !== -1) {
                 mo += minutes;
             }
-            if (course[1].indexOf('Tu') != -1) {
+            if (course[1].indexOf('Tu') !== -1) {
                 tu += minutes;
             }
-            if (course[1].indexOf('We') != -1) {
+            if (course[1].indexOf('We') !== -1) {
                 we += minutes;
             }
-            if (course[1].indexOf('Th') != -1) {
+            if (course[1].indexOf('Th') !== -1) {
                 th += minutes;
             }
-            if (course[1].indexOf('Fr') != -1) {
+            if (course[1].indexOf('Fr') !== -1) {
                 fr += minutes;
             }
         }
 
-        const stdev = math.std([mo, tu, we, th, fr]);
-
         this.schedules.push({
             schedule: timeTable.concat(),
-            coeff: stdev
+            coeff: ScheduleEvaluator.variance([mo, tu, we, th, fr])
         });
     }
 

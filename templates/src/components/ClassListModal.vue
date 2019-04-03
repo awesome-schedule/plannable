c83d094958fd2e356bc2e06a6a14dec762eeda8c
kaiyingshan <shan_kaiying@163.com>
2019-04-03 17:45:23 -0400

    added modal for combined section

--- templates/src/components/ClassListModal.vue
+++ templates/src/components/ClassListModal.vue
@@ -1,11 +1,5 @@
 <template>
-    <div
-        class="modal fade"
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-    >
+    <div class="modal fade" tabindex="-1" role="dialog">
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">

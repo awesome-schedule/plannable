3157e595900f2889c4b4c707c355c1353cf60bfc
hanzhi713 <hanzhi713@163.com>
2019-05-14 22:45:30 -0400

    change timeout to 50s

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -2,5 +2,6 @@ global.console.time = jest.fn();
 global.console.timeEnd = jest.fn();
 global.console.log = jest.fn();
 
-// set async time out to 20 seconds
-jest.setTimeout(20000);
+// set async time out to 50 seconds
+// God knows why sometimes request takes so long
+jest.setTimeout(50000);

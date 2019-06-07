71613e5418c81c16ad2fddd9b50860be3456a40c
hanzhi713 <hanzhi713@163.com>
2019-06-08 00:57:09 +0800

    fix babel plugin injection

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,10 @@
+// transpile all modules to commonjs to avoid interpolation issue
+// https://github.com/vuejs/vue-cli/issues/2637
+const plugins = ['@babel/plugin-transform-modules-commonjs'];
+if (process.env.NODE_ENV === 'production') {
+    plugins.push(['transform-remove-console', { exclude: ['info', 'error', 'warn'] }]);
+}
 module.exports = {
     presets: ['@vue/app'],
-    env: {
-        production: {
-            plugins: [['transform-remove-console', { exclude: ['info', 'error', 'warn'] }]]
-        }
-    },
-
-    // transpile all modules to commonjs to avoid interpolation issue
-    // https://github.com/vuejs/vue-cli/issues/2637
-    plugins: ['@babel/plugin-transform-modules-commonjs']
+    plugins
 };

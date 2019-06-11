227baf4359776d9f8addd2161a32c6487335b322
hanzhi713 <hanzhi713@163.com>
2019-06-11 20:25:25 +0800

    add catalog loader

--- scripts/data_loader.ts
+++ scripts/data_loader.ts
@@ -2,6 +2,7 @@ import cheerio from 'cheerio';
 import { stringify } from 'querystring';
 import axios from 'axios';
 import { SemesterJSON } from '../src/models/Catalog';
+import fs from 'fs';
 
 async function loadSemesterList(count = 5) {
     const { data } = await axios.get('https://rabi.phys.virginia.edu/mySIS/CS2/index.php');
@@ -23,9 +24,27 @@ async function loadSemesterList(count = 5) {
     return records;
 }
 
+async function loadSemesterData(semester: SemesterJSON) {
+    const { data } = await axios.post(
+        `https://rabi.phys.virginia.edu/mySIS/CS2/deliverData.php`, // yes
+        stringify({
+            Semester: semester.id,
+            Group: 'CS',
+            Description: 'Yes',
+            submit: 'Submit Data Request',
+            Extended: 'Yes'
+        })
+    );
+    fs.writeFileSync(`./data/Semester Data/CS${semester.id}Data.csv`, data);
+}
+
 async function main() {
+    console.info('Loading semester list...');
     const semesters = await loadSemesterList();
-    console.log(semesters);
+    for (const semester of semesters) {
+        console.info('Loading', semester.name, 'data...');
+        await loadSemesterData(semester);
+    }
 }
 
 main();

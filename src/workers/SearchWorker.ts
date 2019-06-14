895b9d46d6c916f14deb816b9dd3c0e81dfd403b
kaiyingshan <shan_kaiying@163.com>
2019-06-14 04:34:28 -0400

    merge changes && optimized section normalization

--- src/workers/SearchWorker.ts
+++ src/workers/SearchWorker.ts
@@ -1,7 +1,7 @@
 /**
  * Search worker is used to perform fuzzy search (which is very expensive)
  * in a separate, non-blocking process.
- * @author Hanzhi Zhou
+ * @author Hanzhi Zhou, Kaiying Shan (non-human, cat)
  * @requires optimization
  * @requires fast-fuzzy
  * @see https://github.com/EthanRutherford/fast-fuzzy
@@ -11,7 +11,7 @@
  *
  */
 import { Searcher, SearchResult } from 'fast-fuzzy';
-import Course, { CourseConstructorArguments } from '../models/Course';
+import Course, { CourseConstructorArguments, Match } from '../models/Course';
 import Section, { SectionMatch } from '../models/Section';
 
 declare function postMessage(msg: CourseConstructorArguments[] | 'ready'): void;
@@ -41,15 +41,18 @@ onmessage = (msg: MessageEvent) => {
         // so future searches won't need to re-calculate them on the fly
 
         // fast-fuzzy does not support nested data, so we search for sections and courses separately.
+
         sectionSearcher = new Searcher(sections, {
             returnMatchData: true,
             ignoreCase: true,
+            ignoreSymbols: true,
             normalizeWhitespace: true,
             keySelector: obj => [obj.topic, obj.instructors.join(', ')]
         });
         courseSearcher = new Searcher(courses, {
             returnMatchData: true,
             ignoreCase: true,
+            ignoreSymbols: true,
             normalizeWhitespace: true,
             keySelector: obj => [obj.title, obj.description]
         });
@@ -57,44 +60,64 @@ onmessage = (msg: MessageEvent) => {
         console.timeEnd('worker prep');
     } else {
         const query: string = msg.data;
+        const querySeg: string[] = query.split(' ').filter(x => x.length >= 3);
 
-        const courseResults = courseSearcher.search(query);
-        const sectionResults = sectionSearcher.search(query);
+        let courseResults;
+        let sectionResults;
 
-        const courseScores: { [x: string]: number } = Object.create(null);
-        const courseMap: { [x: string]: SearchResult<Course> } = Object.create(null);
+        const courseScores: { [x: string]: [number, number, number] } = Object.create(null);
+        const courseMap: { [x: string]: SearchResult<Course>[] } = Object.create(null);
         const sectionMap: { [x: string]: SearchResult<Section>[] } = Object.create(null);
+        const sectionRecorder: Set<string> = new Set();
 
-        for (const result of courseResults) {
-            const item = result.item;
-            const key = item.key;
+        for (const q of querySeg) {
+            courseResults = courseSearcher.search(q);
+            sectionResults = sectionSearcher.search(q);
 
-            // penalize desc. match more
-            courseScores[key] = result.score * (result.original === item.title ? 1 : 0.5);
-            courseMap[key] = result;
+            for (const result of courseResults) {
+                const item = result.item;
+                const key = item.key;
+                const score = result.score * (result.original === item.title ? 1 : 0.5);
+                if (courseMap[key]) {
+                    courseScores[key][0] += score;
+                    courseMap[key].push(result);
+                } else {
+                    courseScores[key] = [score, 0, 0];
+                    courseMap[key] = [result];
+                }
+            }
+
+
+
+            for (const result of sectionResults) {
+                const item = result.item;
+                const key = item.key;
+                const score = result.score * (result.original === item.topic ? 0.8 : 0.4);
+
+                if (courseScores[key]) {
+                    courseScores[key][1] += score;
+                } else {
+                    courseScores[key] = [0, score, 0];
+                }
+
+                if (sectionMap[key]) {
+                    sectionMap[key].push(result);
+                } else {
+                    sectionMap[key] = [result];
+                }
+
+                const secKey = `${item.key} ${item.sid}`;
+
+                if (!sectionRecorder.has(secKey)) {
+                    courseScores[key][2] += 1;
+                    sectionRecorder.add(secKey);
+                }
+            }
         }
 
-        for (const result of sectionResults) {
-            const item = result.item;
-            const key = item.key;
-
-            // penalize instructors match more
-            const score = result.score * (result.original === item.topic ? 0.8 : 0.4);
-            if (courseScores[key]) {
-                courseScores[key] += score;
-            } else {
-                courseScores[key] = score;
-            }
-            if (sectionMap[key]) {
-                sectionMap[key].push(result);
-            } else {
-                sectionMap[key] = [result];
-            }
-        }
-
-        // sort by scores, descending order
         const scoreEntries = Object.entries(courseScores)
-            .sort((a, b) => b[1] - a[1])
+            .sort((a, b) => (b[1][0] + (b[1][2] === 0 ? 0 : (b[1][1] / b[1][2])))
+                - (a[1][0] + (a[1][2] === 0 ? 0 : (a[1][1] / a[1][2]))))
             .slice(0, 12);
 
         const finalResults: CourseConstructorArguments[] = [];
@@ -104,7 +127,24 @@ onmessage = (msg: MessageEvent) => {
             const courseMatch = courseMap[key];
             let course: CourseConstructorArguments;
             if (courseMatch) {
-                const { match, original, item } = courseMatch;
+                const { match, original, item } = courseMatch[0];
+
+                const mats: Match<'title' | 'description' | 'key'>[] = courseMatch.map(x => [{
+                    match: x.original === x.item.title ? 'title' : 'description' as 'title' | 'description' | 'key',
+                    start: x.match.index,
+                    end: x.match.index + x.match.length
+                }]).map(x => x[0]);
+
+                // const mats : Match<'title' | 'description' | 'key'>[] = [];
+
+                // for(const cm_i of courseMatch){
+
+                //     for(const cm_j of courseMatch){
+                //         if(cm_i === cm_j) continue;
+
+                //     }
+                // }
+
                 const combSecMatches: SectionMatch[][] = [];
                 const s = sectionMap[key];
 
@@ -131,16 +171,9 @@ onmessage = (msg: MessageEvent) => {
                     item.raw,
                     key,
                     item.sids,
-                    [
-                        {
-                            match: original === item.title ? 'title' : 'description',
-                            start: match.index,
-                            end: match.index + match.length
-                        }
-                    ],
+                    mats,
                     combSecMatches
                 ];
-
                 // only section match exists
             } else {
                 const s = sectionMap[key];

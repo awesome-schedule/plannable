45228e74036309aded3e60b75a18da48c9f6ee76
hanzhi713 <hanzhi713@163.com>
2019-07-02 22:07:33 +0800

    restore evaluator test

--- tests/unit/scheduleEvaluator.spec.ts
+++ tests/unit/scheduleEvaluator.spec.ts
@@ -6,61 +6,55 @@ test('dummy', () => {
     expect(1).toBe(1);
 });
 
-// const schedules: RawAlgoSchedule = [
-//     ['1', [[100, 200], [], [], [], []], [1], [[-1], [], [], [], []]],
-//     ['2', [[50, 80], [], [], [], []], [1], [[-1], [], [], [], []]],
-//     ['3', [[350, 450], [], [], [], []], [1], [[-1], [], [], [], []]],
-//     ['4', [[10, 15], [], [], [], []], [1], [[-1], [], [], [], []]],
-//     ['5', [[], [500, 600, 300, 350], [], [], []], [1], [[], [-1], [], [], []]],
-//     ['6', [[], [250, 300, 100, 200], [], [], []], [1], [[], [-1], [], [], []]]
-// ];
+const schedules: RawAlgoSchedule = [
+    ['1', [1], [[100, 200, -1], [], [], [], []]],
+    ['2', [1], [[50, 80, -1], [], [], [], []]],
+    ['3', [1], [[350, 450, -1], [], [], [], []]],
+    ['4', [1], [[10, 15, -1], [], [], [], []]],
+    ['5', [1], [[], [500, 600, -1, 300, 350, -1], [], [], []]],
+    ['6', [1], [[], [250, 300, -1, 100, 200, -1], [], [], []]]
+];
 
-// describe('Schedule Evaluator Test', () => {
-//     it('Compactness Test', () => {
-//         const evaluator = new ScheduleEvaluator(filter.sortOptions, window.timeMatrix);
-//         evaluator.add(schedules);
-//         const s = evaluator._schedules[0];
-//         const func = evaluator.sortFunctions.compactness.bind(evaluator);
-//         expect(func(s)).toBe(35 + 20 + 150 + 50 + 0 + 150);
-//     });
+describe('Schedule Evaluator Test', () => {
+    it('Compactness Test', () => {
+        const evaluator = new ScheduleEvaluator(filter.sortOptions, window.timeMatrix);
+        evaluator.add(schedules);
+        const s = evaluator._schedules[0];
+        const func = evaluator.sortFunctions.compactness.bind(evaluator);
+        expect(func(s)).toBe(35 + 20 + 150 + 50 + 0 + 150);
+    });
 
-//     it('Insertion Test', () => {
-//         const evaluator = new ScheduleEvaluator(filter.sortOptions, window.timeMatrix);
-//         evaluator.add(schedules);
-//         const s = evaluator._schedules[0];
-//         expect(s.blocks[0]).toEqual([10, 15, 50, 80, 100, 200, 350, 450]);
-//         expect(s.blocks[1]).toEqual([100, 200, 250, 300, 300, 350, 500, 600]);
-//     });
+    it('Insertion Test', () => {
+        const evaluator = new ScheduleEvaluator(filter.sortOptions, window.timeMatrix);
+        evaluator.add(schedules);
+        const s = evaluator._schedules[0];
+        expect(s.blocks[0]).toEqual([10, 15, -1, 50, 80, -1, 100, 200, -1, 350, 450, -1]);
+        expect(s.blocks[1]).toEqual([100, 200, -1, 250, 300, -1, 300, 350, -1, 500, 600, -1]);
+    });
 
-//     it('lunch Test', () => {
-//         expect(1).toBe(1);
-//     });
+    it('lunch Test', () => {
+        expect(1).toBe(1);
+    });
 
-//     it('Sort Option JSON Parse', () => {
-//         const rawSortOptions: EvaluatorOptions = {
-//             sortBy: [
-//                 {
-//                     name: 'distance',
-//                     enabled: false,
-//                     reverse: true
-//                 }
-//             ],
-//             mode: 1
-//         };
-//         const sortOption = filter.sortOptions;
-//         sortOption.fromJSON(rawSortOptions);
-//         expect(sortOption.sortBy[0].enabled).toBe(false);
-//         expect(sortOption.sortBy[0].reverse).toBe(true);
-//         expect(sortOption.mode).toBe(1);
-//         const optionDefaults = filter.sortOptions;
-//         for (let i = 1; i < sortOption.sortBy.length; i++) {
-//             expect(sortOption.sortBy[i]).toEqual(optionDefaults.sortBy[i]);
-//         }
-//     });
-// });
-
-// describe('1', () => {
-//     it('2', () => {
-//         expect(1).toBe(1);
-//     });
-// });
+    it('Sort Option JSON Parse', () => {
+        const rawSortOptions: EvaluatorOptions = {
+            sortBy: [
+                {
+                    name: 'distance',
+                    enabled: false,
+                    reverse: true
+                }
+            ],
+            mode: 1
+        };
+        const sortOption = filter.sortOptions;
+        sortOption.fromJSON(rawSortOptions);
+        expect(sortOption.sortBy[0].enabled).toBe(false);
+        expect(sortOption.sortBy[0].reverse).toBe(true);
+        expect(sortOption.mode).toBe(1);
+        const optionDefaults = filter.sortOptions;
+        for (let i = 1; i < sortOption.sortBy.length; i++) {
+            expect(sortOption.sortBy[i]).toEqual(optionDefaults.sortBy[i]);
+        }
+    });
+});

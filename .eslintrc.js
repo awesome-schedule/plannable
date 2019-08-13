c023f4e1d7c59db56138a58f797b334a86fc4d6f
hanzhi713 <hanzhi713@163.com>
2019-08-13 19:38:36 +0800

    remove conditional flag in eslintrcc

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,8 @@ module.exports = {
     },
     extends: ['plugin:vue/recommended', '@vue/prettier', '@vue/typescript'],
     rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-console': 'off',
+        'no-debugger': 'error',
         'prefer-const': 'error',
         'vue/require-default-prop': 'off',
         'no-var': 'error',

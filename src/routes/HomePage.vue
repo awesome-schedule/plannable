8ecce4a6e5721f64d9ecc46093605ca6096b4de4
Zichao <zichao2018@gmail.com>
2019-08-16 08:41:16 +0800

    temp navbar

--- src/routes/HomePage.vue
+++ src/routes/HomePage.vue
@@ -1,23 +1,42 @@
-<template class='bg-primary'>
-    <div class="container">
-        <h1 class="display-2">Welcome to Plannable</h1>
-        <img class="img-fluid" src="../assets/cover.jpg" />
-        <hr class="my-4" />
-        <p>
-            Provide students with free and handy tools to schedule their classes more efficiently.
-        </p>
-        <div>
-            <select v-model="currentSelect" class="custom-select custom-select-sm py-0">
-                <option v-for="(item, index) in schoolNames" :key="index" :value="index">{{
-                    item
-                }}</option>
-            </select>
+<template>
+    <div>
+        <nav  class="navbar navbar-expand navbar-light bg-light flex-column flex-md-row bd-navbar">
+            <div class="navbar-nav-scroll">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/contact">Contact/Join Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/contact">Advertise With Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/contact">Check Previous Updates</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
-            <button class="btn btn-primary" @click="redirection">
-                Go to
-            </button>
+        <div class="container">
+            <h1 class="display-2">Welcome to Plannable</h1>
+            <img class="img-fluid" src="../assets/cover.jpg" />
+            <hr class="my-4" />
+            <p>
+                Provide students with free and handy tools to schedule their classes more
+                efficiently.
+            </p>
+            <div>
+                <select v-model="currentSelect" class="custom-select custom-select-sm py-0">
+                    <option v-for="(item, index) in schoolNames" :key="index" :value="index">{{
+                        item
+                    }}</option>
+                </select>
+
+                <button class="btn btn-primary" @click="redirection">
+                    Go to
+                </button>
+            </div>
+            <div id="release-note" class="container"></div>
         </div>
-        <div id="release-note"></div>
     </div>
 </template>
 

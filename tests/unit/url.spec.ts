20c1f0ccb0da31222093fe18cbfd6cb3aeba4c10
hanzhi713 <hanzhi713@163.com>
2019-09-12 17:31:39 -0400

    impl. new match func to url spec

--- tests/unit/url.spec.ts
+++ tests/unit/url.spec.ts
@@ -1,6 +1,6 @@
 import lz from 'lz-string';
 
-import { compressJSON, parseFromURL } from '@/store';
+import Store, { compressJSON, parseFromURL, SemesterStorage } from '@/store';
 import empty_schedule from './test_data/empty_schedule.json';
 import miscellaneousTest1 from './test_data/miscellaneousTest1.json';
 import mySchedule2019Fall from './test_data/mySchedule2019Fall.json';
@@ -293,29 +293,60 @@ describe('url convertJsonToArray test', () => {
     });
 });
 
+function matchPartial(input: SemesterStorage, target: SemesterStorage) {
+    const { schedule, ...others } = input;
+    const { schedule: T_schedule, ...T_others } = target;
+
+    expect(others).toEqual(T_others);
+    const { proposedSchedules, ...others2 } = schedule;
+    const { proposedSchedules: T_proposedSchedules, ...T_others2 } = T_schedule;
+
+    expect(others2).toEqual(T_others2);
+    expect(proposedSchedules.length).toBe(T_proposedSchedules.length);
+    for (let i = 0; i < proposedSchedules.length; i++) {
+        const { events, All } = proposedSchedules[i];
+        const { events: T_events, All: T_All } = T_proposedSchedules[i];
+        expect(T_events).toEqual(events);
+        for (const [k, v] of Object.entries(All)) {
+            // input may lose some keys due to course removal
+            const secs = v,
+                T_secs = T_All[k];
+            if (secs instanceof Array) {
+                expect(T_secs).toBeInstanceOf(Array);
+                if (T_secs === -1) throw new Error(); // can never be -1
+                for (const sec of secs) {
+                    expect(T_secs.find(s => s.id === sec.id)).toBeTruthy(); // expect to find in T_secs
+                }
+            } else {
+                expect(secs).toBe(T_secs); // -1 case
+            }
+        }
+    }
+}
+
 describe('parseFromURL', () => {
     it('parse_empty_schedule', async () => {
         const urlCompressed = compressJSON(JSON.stringify(empty_schedule));
         const URL = lz.compressToEncodedURIComponent(JSON.stringify(urlCompressed));
         const json = await parseFromURL(URL);
-        expect(JSON.parse(JSON.stringify(json))).toEqual(empty_schedule);
+        matchPartial(JSON.parse(JSON.stringify(json)), empty_schedule as any);
     });
     it('parse_mySchedule2019Fall', async () => {
         const urlCompressed = compressJSON(JSON.stringify(mySchedule2019Fall));
         const URL = lz.compressToEncodedURIComponent(JSON.stringify(urlCompressed));
         const json = await parseFromURL(URL);
-        expect(JSON.parse(JSON.stringify(json))).toEqual(mySchedule2019Fall);
+        matchPartial(JSON.parse(JSON.stringify(json)), mySchedule2019Fall as any);
     });
     it('parse_test_filter', async () => {
         const urlCompressed = compressJSON(JSON.stringify(test_filter));
         const URL = lz.compressToEncodedURIComponent(JSON.stringify(urlCompressed));
         const json = await parseFromURL(URL);
-        expect(JSON.parse(JSON.stringify(json))).toEqual(test_filter);
+        matchPartial(JSON.parse(JSON.stringify(json)), test_filter as any);
     });
     it('parse_miscellaneousTest1', async () => {
         const urlCompressed = compressJSON(JSON.stringify(miscellaneousTest1));
         const URL = lz.compressToEncodedURIComponent(JSON.stringify(urlCompressed));
         const json = await parseFromURL(URL);
-        expect(JSON.parse(JSON.stringify(json))).toEqual(miscellaneousTest1);
+        matchPartial(JSON.parse(JSON.stringify(json)), miscellaneousTest1 as any);
     });
 });

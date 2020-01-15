18bcc6e21e6c0ed6a5902dacde769eb585d2220d
hanzhi713 <hanzhi713@163.com>
2020-01-14 22:21:00 -0500

    tokenized search worker

--- src/workers/SearchWorker.ts
+++ src/workers/SearchWorker.ts
@@ -33,7 +33,8 @@ interface SearchResult<T, K> {
  * Fast searcher for fuzzy search among a list of strings
  */
 class FastSearcher<T, K = string> {
-    public targets: string[];
+    public targets: string[][];
+    public indices: Uint16Array[];
     public items: T[];
     public data: K;
     /**
@@ -41,43 +42,67 @@ class FastSearcher<T, K = string> {
      */
     constructor(targets: T[], toStr: (a: T) => string, data: K) {
         this.items = targets;
-        this.targets = targets.map(
-            t => toStr(t).toLowerCase()
-            // .replace(/\s+/g, '')
-        );
+        this.indices = [];
+        this.targets = targets.map(t => {
+            const full = toStr(t).toLowerCase();
+            const temp = full.split(/\s+/);
+            const idx = new Uint16Array(temp.length + 1);
+            for (let i = 0; i < temp.length; i++) {
+                idx[i] = full.indexOf(temp[i]);
+            }
+            idx[temp.length] = full.length;
+            this.indices.push(idx);
+            return temp;
+        });
         this.data = data;
     }
     public search(first: string) {
         first = first.toLowerCase().replace(/\s+/g, '');
-        const targetStrings = this.targets;
-        const len = targetStrings.length;
+
+        const temp = new Map<string, number>();
+        const len1 = first.length;
+        for (let j = 0; j < len1 - 1; j++) {
+            const bigram = first.substring(j, j + 2);
+            temp.set(bigram, 1 + (temp.get(bigram) || 0));
+        }
+
         const allMatches: SearchResult<T, K>[] = [];
-        const firstBigrams = new Map<string, number>();
-        for (let i = 0; i < len; i++) {
-            const second = targetStrings[i];
-            const len1 = first.length,
-                len2 = second.length;
+        for (let i = 0; i < this.targets.length; i++) {
             // if (!len1 && !len2) return [1, [0, 0]] as const; // if both are empty strings
             // if (!len1 || !len2) return [0, []] as const; // if only one is empty string
             // if (first === second) return [1, [0, len1]] as const; // identical
             // if (len1 === 1 && len2 === 1) return [0, []] as const; // both are 1-letter strings
             // if (len1 < 2 || len2 < 2) return [0, []] as const; // if either is a 1-letter string
 
-            for (let j = 0; j < len1 - 1; j++) {
-                const bigram = first.substring(j, j + 2);
-                firstBigrams.set(bigram, 1 + (firstBigrams.get(bigram) || 0));
-            }
-
-            let intersectionSize = 0;
             const matches = [];
-            for (let j = 0; j < len2 - 1; j++) {
-                const bigram = second.substring(j, j + 2);
-                const count = firstBigrams.get(bigram) || 0;
+            const tokens = this.targets[i];
+            const indices = this.indices[i];
+            let score = 0;
+            for (let k = 0; k < tokens.length; k++) {
+                const token = tokens[k];
+                const start = indices[k];
+                let intersectionSize = 0;
+                const len2 = token.length;
+                const firstBigrams = new Map(temp);
 
-                if (count > 0) {
-                    firstBigrams.set(bigram, count - 1);
-                    intersectionSize++;
-                    matches.push(j, j + 2);
+                for (let j = 0; j < len2 - 1; j++) {
+                    const bigram = token.substring(j, j + 2);
+                    const count = firstBigrams.get(bigram) || 0;
+
+                    if (count > 0) {
+                        firstBigrams.set(bigram, count - 1);
+                        intersectionSize++;
+                        matches.push(start + j, start + j + 2);
+                    }
+                }
+                if (intersectionSize < 2) {
+                    matches.splice(matches.length - intersectionSize * 2);
+                    continue;
+                }
+
+                const t1 = (2 * intersectionSize) / (len1 + len2 - 2);
+                if (t1 > score) {
+                    score = t1;
                 }
             }
 
@@ -95,13 +120,12 @@ class FastSearcher<T, K = string> {
             // }
 
             allMatches.push({
-                score: (2 * intersectionSize) / (len1 + len2 - 2),
+                score,
                 matches,
                 item: this.items[i],
                 index: i,
                 data: this.data
             });
-            firstBigrams.clear();
         }
         return allMatches.sort((a, b) => b.score - a.score);
     }
@@ -300,5 +324,5 @@ function toMatches(matches: SearchResult<any, any>[]) {
             });
         }
     }
-    return resolveOverlap(allMatches).filter(x => x.end - x.start > 2);
+    return resolveOverlap(allMatches);
 }

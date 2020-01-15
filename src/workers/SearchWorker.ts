bf7b5b00c7830c07e82004c5bac393ba3d154820
Hanzhi Zhou <hanzhi713@163.com>
2020-01-14 20:10:45 -0500

    dice based fuzzy search

--- src/workers/SearchWorker.ts
+++ src/workers/SearchWorker.ts
@@ -12,143 +12,170 @@
  */
 import { RawAlgoCourse } from '@/algorithm/ScheduleGenerator';
 import { SearchMatch } from '@/models/Catalog';
-import _Course, { CourseMatch } from '../models/Course';
+import _Course, { CourseMatch, Match } from '../models/Course';
 import _Meeting from '../models/Meeting';
 import { SectionFields, SectionMatch } from '../models/Section';
-import { blockUnion } from '@/utils';
+import { calcOverlap } from '@/utils';
 
 type Section = Omit<SectionFields, 'course'>;
 interface Course extends Omit<NonFunctionProperties<_Course>, 'sections'> {}
 
 declare function postMessage(msg: [RawAlgoCourse[], SearchMatch[]] | 'ready'): void;
 
-/**
- * @license MIT
- * Adapted from [[https://github.com/aceakash/string-similarity]], with optimizations:
- * 1. Cache the string length
- * 2. No need to use .has for map
- * 3. Pre-process the target list of strings for repeated searches (see [[FastSearcher]])
- * About 40% faster than the original one.
- * @param first
- * @param second
- */
-function findBestMatch(first: string, targetStrings: string[]) {
-    const len = targetStrings.length;
-    const allMatches: number[][] = [];
-    const firstBigrams = new Map<string, number>();
-    for (let i = 0; i < len; i++) {
-        const second = targetStrings[i];
-        const len1 = first.length,
-            len2 = second.length;
-        // if (!len1 && !len2) return [1, [0, 0]] as const; // if both are empty strings
-        // if (!len1 || !len2) return [0, []] as const; // if only one is empty string
-        // if (first === second) return [1, [0, len1]] as const; // identical
-        // if (len1 === 1 && len2 === 1) return [0, []] as const; // both are 1-letter strings
-        // if (len1 < 2 || len2 < 2) return [0, []] as const; // if either is a 1-letter string
-
-        for (let j = 0; j < len1 - 1; j++) {
-            const bigram = first.substring(j, j + 2);
-            firstBigrams.set(bigram, 1 + (firstBigrams.get(bigram) || 0));
-        }
-
-        let intersectionSize = 0;
-        const matches = [0.0, i];
-        for (let j = 0; j < len2 - 1; j++) {
-            const bigram = second.substring(j, j + 2);
-            const count = firstBigrams.get(bigram) || 0;
-
-            if (count > 0) {
-                firstBigrams.set(bigram, count - 1);
-                intersectionSize++;
-                matches.push(j, j + 2);
-            }
-        }
-
-        matches[0] = (2 * intersectionSize) / (len1 + len2 - 2);
-        for (let j = 2; j < matches.length - 2; j += 2) {
-            const union = blockUnion(matches[j], matches[j + 1], matches[j + 2], matches[j + 3]);
-            if (union) {
-                matches.splice(j, 4, union[0], union[1]);
-                j -= 2;
-            }
-        }
-        allMatches.push(matches);
-        firstBigrams.clear();
-    }
-    return allMatches.sort((a, b) => b[0] - a[0]);
+interface SearchResult<T, K> {
+    score: number;
+    matches: number[];
+    index: number;
+    item: T;
+    data: K;
 }
 /**
  * Fast searcher for fuzzy search among a list of strings
  */
-class FastSearcher<T> {
+class FastSearcher<T, K = string> {
     public targets: string[];
+    public items: T[];
+    public data: K;
     /**
      * @param targets the list of strings to search from
      */
-    constructor(targets: T[], toStr: (a: T) => string) {
+    constructor(targets: T[], toStr: (a: T) => string, data: K) {
+        this.items = targets;
         this.targets = targets.map(
             t => toStr(t).toLowerCase()
             // .replace(/\s+/g, '')
         );
+        this.data = data;
     }
-    public search(query: string) {
-        const arr = findBestMatch(query.toLowerCase().replace(/\s+/g, ''), this.targets);
-        // for (let i = 0; i < 5; i++) {
-        //     const temp = arr[i];
-        //     const str = this.targets[temp[1]];
-        //     let l = '';
-        //     for (let j = 2; j < temp.length; j += 2) {
-        //         l += str.substring(temp[j], temp[j + 1]) + ' ';
-        //     }
-        //     console.log(l);
-        // }
-        // console.log(arr);
-        return arr;
+    public search(first: string) {
+        first = first.toLowerCase().replace(/\s+/g, '');
+        const targetStrings = this.targets;
+        const len = targetStrings.length;
+        const allMatches: SearchResult<T, K>[] = [];
+        const firstBigrams = new Map<string, number>();
+        for (let i = 0; i < len; i++) {
+            const second = targetStrings[i];
+            const len1 = first.length,
+                len2 = second.length;
+            // if (!len1 && !len2) return [1, [0, 0]] as const; // if both are empty strings
+            // if (!len1 || !len2) return [0, []] as const; // if only one is empty string
+            // if (first === second) return [1, [0, len1]] as const; // identical
+            // if (len1 === 1 && len2 === 1) return [0, []] as const; // both are 1-letter strings
+            // if (len1 < 2 || len2 < 2) return [0, []] as const; // if either is a 1-letter string
+
+            for (let j = 0; j < len1 - 1; j++) {
+                const bigram = first.substring(j, j + 2);
+                firstBigrams.set(bigram, 1 + (firstBigrams.get(bigram) || 0));
+            }
+
+            let intersectionSize = 0;
+            const matches = [];
+            for (let j = 0; j < len2 - 1; j++) {
+                const bigram = second.substring(j, j + 2);
+                const count = firstBigrams.get(bigram) || 0;
+
+                if (count > 0) {
+                    firstBigrams.set(bigram, count - 1);
+                    intersectionSize++;
+                    matches.push(j, j + 2);
+                }
+            }
+
+            // for (let j = 0; j < matches.length; j += 2) {
+            //     const union = blockUnion(
+            //         matches[j],
+            //         matches[j + 1],
+            //         matches[j + 2],
+            //         matches[j + 3]
+            //     );
+            //     if (union) {
+            //         matches.splice(j, 4, union[0], union[1]);
+            //         j -= 2;
+            //     }
+            // }
+
+            allMatches.push({
+                score: (2 * intersectionSize) / (len1 + len2 - 2),
+                matches,
+                item: this.items[i],
+                index: i,
+                data: this.data
+            });
+            firstBigrams.clear();
+        }
+        return allMatches.sort((a, b) => b.score - a.score);
     }
     public toJSON() {
         return this.targets;
     }
 }
 
-// function processCourseResults(
-//     results: SearchResult<Course>[],
-//     courseMap: CourseMap,
-//     scores: Scores,
-//     match: 'title' | 'description',
-//     weight: number
-// ) {
-//     for (const result of results) {
-//         const key = result.item.key;
-//         const score = result.score ** 3 * weight;
-//         const tempObj = {
-//             result,
-//             match
-//         };
-//         if (courseMap[key]) {
-//             scores[key][0] += score;
-//             courseMap[key].push(tempObj);
-//         } else {
-//             // if encounter this course for the first time
-//             scores[key] = [score, 0, 0];
-//             courseMap[key] = [tempObj];
-//         }
-//     }
-// }
+let titleSearcher: FastSearcher<Course>;
+let descriptionSearcher: FastSearcher<Course>;
+let topicSearcher: FastSearcher<Section>;
+let instrSearcher: FastSearcher<Section>;
 
-interface ResultMap {
-    [x: string]: number[];
-}
-interface Scores {
-    /**
-     * elements in array:
-     * 1. score for courses,
-     * 2. score for sections,
-     * 3. number of distinct sections
-     */
-    [x: string]: [number, number, number];
+function processCourseResults(results: SearchResult<Course, string>[], weight: number) {
+    for (const result of results) {
+        const key = result.item.key;
+        const score = result.score ** 3 * weight;
+
+        const temp = courseMap.get(key);
+        if (temp) {
+            scores.get(key)![0] += score;
+            temp.push(result);
+        } else {
+            // if encounter this course for the first time
+            scores.set(key, [score, 0, 0]);
+            courseMap.set(key, [result]);
+        }
+    }
 }
 
-const searchers: FastSearcher<any>[] = [];
+function processSectionResults(results: SearchResult<Section, string>[], weight: number) {
+    for (const result of results) {
+        const item = result.item;
+        const key = item.key;
+        const score = result.score ** 3 * weight;
+
+        let scoreEntry = scores.get(key);
+        if (!scoreEntry) {
+            scoreEntry = [0, 0, 0];
+            scores.set(key, scoreEntry);
+        }
+        scoreEntry[1] += score;
+
+        const secMatches = sectionMap.get(key);
+        if (secMatches) {
+            const matches = secMatches.get(item.id);
+            if (matches) {
+                matches.push(result);
+            } else {
+                secMatches.set(item.id, [result]);
+                // if encounter a new section of a course, increment the number of section recorded
+                scoreEntry[2] += 1;
+            }
+        } else {
+            sectionMap.set(key, new Map().set(item.id, [result]));
+            scoreEntry[2] += 1;
+        }
+    }
+}
+
+type CourseResultMap = Map<string, SearchResult<Course, string>[]>;
+type SectionResultMap = Map<string, Map<number, SearchResult<Section, string>[]>>;
+/**
+ * elements in array:
+ * 1. score for courses,
+ * 2. score for sections,
+ * 3. number of distinct sections
+ */
+type ScoreEntry = [number, number, number];
+type Scores = Map<string, ScoreEntry>;
+
+const courseMap: CourseResultMap = new Map();
+const sectionMap: SectionResultMap = new Map();
+const scores: Scores = new Map();
 
 let courses: Course[];
 let sections: Section[];
@@ -164,12 +191,11 @@ onmessage = ({ data }: { data: [Course[], Section[]] | string }) => {
     if (typeof data !== 'string') {
         console.time('worker prep');
         [courses, sections] = data;
-        searchers.push(
-            new FastSearcher(courses, obj => obj.title),
-            new FastSearcher(courses, obj => obj.description),
-            new FastSearcher(sections, obj => obj.topic),
-            new FastSearcher(sections, obj => obj.instructors.join(' '))
-        );
+
+        titleSearcher = new FastSearcher(courses, obj => obj.title, 'title');
+        descriptionSearcher = new FastSearcher(courses, obj => obj.description, 'description');
+        topicSearcher = new FastSearcher(sections, obj => obj.topic, 'topic');
+        instrSearcher = new FastSearcher(sections, obj => obj.instructors.join(' '), 'instructors');
 
         postMessage('ready');
         console.timeEnd('worker prep');
@@ -178,48 +204,19 @@ onmessage = ({ data }: { data: [Course[], Section[]] | string }) => {
         const querySeg: string[] = query.split(/ +/).filter(x => x.length >= 3);
         querySeg.push(query);
 
-        const resultMaps: ResultMap[] = [
-            Object.create(null),
-            Object.create(null),
-            Object.create(null),
-            Object.create(null)
-        ];
-        const targets = [courses, courses, sections, sections] as const;
         for (let j = 0; j < querySeg.length; j++) {
             const q = querySeg[j];
-            // map search result to course (or section) and record the match score
-            for (let i = 0; i < 4; i++) {
-                const results = searchers[i].search(q);
-                const rMap = resultMaps[i];
-                const target = targets[i];
-                for (const result of results) {
-                    const idx = result[1];
-                    const item = target[idx];
-                    if (rMap[item.key]) {
-                        rMap[item.key] = result;
-                    } else {
-                        rMap[item.key] = result;
-                    }
-                }
-            }
-        }
+            // matching the whole query sentence would result in a higher score
+            const weight = +(j === querySeg.length - 1) * 2;
 
-        const weights = [1, 0.5, 0.8, 0.4] as const;
-        const fields = ['title', 'description'];
-
-        const scores: Scores = Object.create(null);
-        for (let i = 0; i < 2; i++) {
-            const rMap = resultMaps[i];
-            const target = targets[i];
-            for (const key in rMap) {
-                const score = scores[key] || [0, 0, 0];
-                score[0] += rMap[key][0] * weights[i];
-                scores[key] = score;
-            }
+            processCourseResults(titleSearcher.search(q), weight);
+            processCourseResults(descriptionSearcher.search(q), 0.5 * weight);
+            processSectionResults(topicSearcher.search(q), 0.8 * weight);
+            processSectionResults(instrSearcher.search(q), 0.4 * weight);
         }
 
         // sort courses in descending order; section score is normalized before added to course score
-        const scoreEntries = Object.entries(scores)
+        const scoreEntries = Array.from(scores.entries())
             .sort(
                 (a, b) =>
                     b[1][0] -
@@ -234,67 +231,74 @@ onmessage = ({ data }: { data: [Course[], Section[]] | string }) => {
 
         // merge course and section matches
         for (const [key] of scoreEntries) {
-            const crsMatches: CourseMatch[] = [];
-            for (let i = 0; i < 2; i++) {
-                const matches = resultMaps[i][key];
-                const field = fields[i];
-                if (matches) {
-                    for (let i = 2; i < matches.length; i += 2) {
-                        crsMatches.push({
-                            match: field as any,
-                            start: matches[i],
-                            end: matches[i + 1]
-                        });
+            const courseMatch = courseMap.get(key);
+            const secMatches = new Map<number, SectionMatch[]>();
+            const s = sectionMap.get(key);
+            if (courseMatch) {
+                const crsMatches: CourseMatch[] = toMatches(courseMatch);
+                // if the section matches for this course exist
+                if (s) {
+                    for (const [id, matches] of s) {
+                        secMatches.set(id, toMatches(matches));
                     }
                 }
+                finalResults.push([key, courseMatch[0].item.ids]);
+                allMatches.push([crsMatches, secMatches]);
+                // only section match exists
+            } else {
+                // if the section matches for this course exist
+                if (s) {
+                    for (const [id, matches] of s) {
+                        secMatches.set(id, toMatches(matches));
+                    }
+                }
+                finalResults.push([key, [...secMatches.keys()]]);
+                allMatches.push([[], secMatches]);
             }
-            if (crsMatches.length) {
-                // const crs = courses[(resultMaps[0][key] || resultMaps[1][key])[0][1]];
-                finalResults.push([key, []]);
-                allMatches.push([crsMatches, new Map()]);
-            }
-
-            // if (courseMatch) {
-            //     const secMatches = new Map<number, SectionMatch[]>();
-            //     const s = sectionMap[key];
-            //     // if the section matches for this course exist
-            //     if (s) {
-            //         for (const [id, matches] of s) {
-            //             secMatches.set(
-            //                 id,
-            //                 matches.map(({ match, result: { match: m } }) => ({
-            //                     match,
-            //                     start: m.index,
-            //                     end: m.index + m.length
-            //                 }))
-            //             );
-            //         }
-            //     }
-            //     finalResults.push([key, courseMatch[0].result.item.ids]);
-            //     allMatches.push([crsMatches, secMatches]);
-
-            //     // only section match exists
-            // } else {
-            //     const secMatches = new Map<number, SectionMatch[]>();
-            //     const s = sectionMap[key];
-            //     if (s) {
-            //         const ids = [...s.keys()];
-            //         for (const [id, matches] of s) {
-            //             secMatches.set(
-            //                 id,
-            //                 matches.map(({ match, result: { match: m } }) => ({
-            //                     match,
-            //                     start: m.index,
-            //                     end: m.index + m.length
-            //                 }))
-            //             );
-            //         }
-            //         finalResults.push([key, ids]);
-            //         allMatches.push([[], secMatches]);
-            //     }
-            // }
         }
-        console.log([finalResults, allMatches]);
+        console.log(finalResults, allMatches);
         postMessage([finalResults, allMatches]);
+
+        courseMap.clear();
+        sectionMap.clear();
+        scores.clear();
     }
 };
+
+function resolveOverlap(arr: Match<any>[]) {
+    arr.sort((a, b) => a.start - b.start);
+    for (let i = 0; i < arr.length - 1; i++) {
+        let j = i + 1;
+        const a = arr[i];
+        let b = arr[j];
+
+        while (a.match !== b.match && j + 1 < arr.length) {
+            b = arr[++j];
+        }
+
+        if (a.match !== b.match) continue;
+        const ovlp = calcOverlap(a.start, a.end, b.start, b.end);
+        if (ovlp > 0) {
+            const prevStart = a.start;
+            (a as any).start = Math.min(a.start, b.start);
+            (a as any).end = a.end - prevStart + a.start + b.end - b.start - ovlp;
+            arr.splice(j, 1);
+            i--;
+        }
+    }
+    return arr;
+}
+
+function toMatches(matches: SearchResult<any, any>[]) {
+    const allMatches: Match<any>[] = [];
+    for (const { data, matches: m } of matches) {
+        for (let i = 0; i < m.length; i += 2) {
+            allMatches.push({
+                match: data as any,
+                start: m[i],
+                end: m[i + 1]
+            });
+        }
+    }
+    return resolveOverlap(allMatches).filter(x => x.end - x.start > 2);
+}

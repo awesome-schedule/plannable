a4e006147644f23680739bebf32489609dc71846
hanzhi713 <hanzhi713@163.com>
2022-04-06 23:18:58 -0400

    update ci scripts

--- scripts/ci_common.sh
+++ scripts/ci_common.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 npm install -g codecov
 npm ci
+npm run updatedata
 npm run getwasm
+
+# http server neededfor testing
+cd scripts
+npx http-server -p 8000 --cors --silent &
+cd ..
+
 npm run test
 npm run build
